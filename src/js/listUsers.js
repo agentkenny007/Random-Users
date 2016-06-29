f33e6f16d22a724c2f874e223eb924baac1ab2eb
Ikenna <ikennaugwuh@gmail.com>
2016-06-29 09:52:29 -0400

    Fixed an oversight.

--- src/js/listUsers.js
+++ src/js/listUsers.js
@@ -4,7 +4,8 @@ var user = $('<li class="user">'), users = $('.random-users');
 var listUsers = function(data){
     console.log(data);
     data.results.forEach(function(result){
-        user.append(`
+        var u = user.clone();
+        u.append(`
             <img src="${result.picture.large}">
             <span class="name">${result.name.first} ${result.name.last}</span>
             <span class="email">${result.email}</span>
@@ -15,7 +16,7 @@ var listUsers = function(data){
             <span class="tel">Home: ${result.phone}</span>
             <span class="cell">Cell: ${result.cell}</span>
             <span class="SSN">${result.id.name}: <span>${result.id.value}</span></span>`);
-        users.append(user);
+        users.append(u); // for user
     });
 };
 
